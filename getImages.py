27d5312d0900594b483a381a686453b17211bcd5
Joe Colburn <joe@joetech.com>
2023-09-19 02:44:33 -0400

    split and merge work, but recursive split fails after the first recursion in depth > 2

--- getImages.py
+++ getImages.py
@@ -1,19 +1,22 @@
 import os
 import sys
 from PIL import Image
+import re
+import webuiapi # https://github.com/mix1009/sdwebuiapi
 
-
-def create_images(prompt):
-    print("Hitting the local Stable Diffusion for the base image. This may take a minute ...")
-
-    api = webuiapi.WebUIApi(host='127.0.0.1', port=7860)
-
+def listModels():
     # List available models 
     models = api.util_get_model_names()
     print(models)
 
+
+def createBaseImage(prompt):
+    print("Hitting the local Stable Diffusion for the base image. This may take a minute ...")
+
+    api = webuiapi.WebUIApi(host='127.0.0.1', port=7860)
+
     # Select a model
-    api.util_set_model('realistic')
+    api.util_set_model('sdvn6Realxl_detailface')
 
     # wait for job complete
     api.util_wait_for_ready()
@@ -37,14 +40,51 @@ def create_images(prompt):
     # Handle the resulting images by saving them to ./images
     imgnum=1
     for i in result.images:
-        filename=f"./tmp/{imgnum}.png"
+        filename=f"./images/tmp.png"
         i.save(filename)
         imgnum+=1
 
 
 def replaceImage(fileName, prompt):
-    # Modify the image based on the prompt and save it
-    pass
+    print(f"Hitting the local Stable Diffusion for the img2img work on {fileName}...")
+
+    api = webuiapi.WebUIApi(host='127.0.0.1', port=7860)
+
+    image = Image.open(f"./images/{fileName}")
+
+    # Select a model
+    api.util_set_model('sdvn6Realxl_detailface')
+
+    # wait for job complete
+    api.util_wait_for_ready()
+
+    # print(f"Prompt:\n{prompt}\n")
+
+    result = api.img2img(prompt=prompt,
+                        images=[image],
+                        negative_prompt="nsfw, nude, naked",
+                        seed=-1,
+                        cfg_scale=7,
+                        n_iter=1,
+                        sampler_name='DPM++ 2M Karras',
+                        steps=20,
+                        width=512,
+                        height=512,
+                        denoising_strength=0.45,
+                        resize_mode=1,
+                        inpainting_fill=0,
+                        )
+
+    # uncomment for debugging
+    # print(result);
+
+    # Handle the resulting images by saving them to ./images
+    imgnum=1
+    for i in result.images:
+        savefile = f"./images/{fileName}"
+        # savefile.replace(".png", f".-opt-{imgnum}.png") # uncomment if n_iter is > 1
+        i.save(savefile)
+        imgnum+=1
 
 
 def splitImage(fileName, numberOfParts, depth, prompts):
@@ -58,27 +98,88 @@ def splitImage(fileName, numberOfParts, depth, prompts):
     print(f"Splitting image {fileName} into {numberOfParts} parts.")
     image = Image.open(imagesDir + fileName)
     w, h = image.size
-    split_size = int(numberOfParts ** 0.5)
-    part_w = w // split_size
-    part_h = h // split_size
+    
+    # Determine rows and columns
+    cols = int(numberOfParts ** 0.5)
+    rows = cols
+    
+    if cols * rows != numberOfParts:
+        # Adjust for non-square numbers
+        if numberOfParts % cols == 0:
+            rows = numberOfParts // cols
+        else:
+            cols = numberOfParts // rows
 
+    part_w = w // cols
+    part_h = h // rows
 
-    for i in range(split_size):
-        for j in range(split_size):
+    for i in range(rows):
+        for j in range(cols):
             part = image.crop((j * part_w, i * part_h, (j + 1) * part_w, (i + 1) * part_h))
 
             # Save each part using the given naming convention
-            partFileName = fileName.replace(".png", f".{i * split_size + j + 1}.png")
+            partFileName = fileName.replace(".png", f".{i * cols + j + 1}.png")
             part.save(imagesDir + partFileName)
 
-            # Replace the image part (assuming replaceImage function is defined elsewhere)
+            # Replace the image part
             replaceImage(partFileName, currentPrompt)
 
-            # Recursive call (if depth is not zero and prompts is not empty)
+            # Recursive call
             if depth > 1 and prompts:
                 splitImage(partFileName, numberOfParts, depth-1, prompts)
 
     currentDepth += 1
+    output = mergeImage(fileName, depth)
+    print(f"Merged image saved as: {output}")
+
+
+
+def mergeImage(baseImage, depth):
+    assert depth > 1, "Depth must be greater than 1"
+    
+    imagesDir = "./images/"
+    
+    # Construct the search pattern based on baseImage and depth
+    prefix = baseImage.replace(".png", "")
+    pattern = re.escape(prefix) + r"\.\d+\.png" if depth > 2 else re.escape(prefix) + r"\.\d+\.png"
+    
+    # List all images in the directory and filter based on the pattern
+    all_files = os.listdir(imagesDir)
+    matching_files = [f for f in all_files if re.match(pattern, f)]
+
+    # Custom sort function to sort based on numerical value after prefix
+    def custom_sort(filename):
+        # Extract the number after the prefix for sorting
+        return int(filename.split('.')[-2])
+
+    matching_files = sorted(matching_files, key=custom_sort)
+    
+    # Calculate the dimensions for the merged image
+    sample_image = Image.open(imagesDir + matching_files[0])
+    w, h = sample_image.size
+    merged_width = int(len(matching_files) ** 0.5 * w)
+    merged_height = int(len(matching_files) ** 0.5 * h)
+    
+    # Create an empty image to paste the smaller images into
+    merged_image = Image.new('RGB', (merged_width, merged_height))
+    
+    # Split size calculation
+    split_size = int(len(matching_files) ** 0.5)
+    
+    for idx, filename in enumerate(matching_files):
+        image = Image.open(imagesDir + filename)
+        
+        x_offset = (idx % split_size) * w
+        y_offset = (idx // split_size) * h
+        
+        merged_image.paste(image, (x_offset, y_offset))
+    
+    # Save or return the merged image (you can modify as needed)
+    # output_filename = prefix + "_merged.png"
+    output_filename = prefix + ".png"
+    merged_image.save(imagesDir + output_filename)
+    
+    return output_filename
 
 
 
@@ -86,57 +187,12 @@ def splitImage(fileName, numberOfParts, depth, prompts):
 
 prompt = "abstract splash of color cyberpunk woman, highly detailed, sharp focus, realistic, masterpiece, amazing, colorful, glistening"
 
-#createBaseImage(prompt)
-splitImage("1.png", 4, 4, [
-    "abstract splash of color cyberpunk man, highly detailed, sharp focus, realistic, masterpiece, amazing, colorful, glistening",
-    "abstract splash of color cyberpunk dog, highly detailed, sharp focus, realistic, masterpiece, amazing, colorful, glistening",
-    "abstract splash of color cyberpunk cat, highly detailed, sharp focus, realistic, masterpiece, amazing, colorful, glistening",
+# createBaseImage(prompt)
+
+splitImage("1.png", 4, 3, [
+    "abstract splash of color cyberpunk world, highly detailed, sharp focus, realistic, masterpiece, amazing, colorful, glistening",
+    "abstract splash of color cyberpunk woman, highly detailed, sharp focus, realistic, masterpiece, amazing, colorful, glistening"
 ])
 
-
-
-
-
-# n = len(sys.argv)
-# img_name = 'input.png'
-# dimY = 2
-# dimX = 2
-# fps = dimY*dimX
-
-# if n >= 3:
-#     dimY = int(sys.argv[1])
-#     dimX = int(sys.argv[2])
-#     if n >= 4:
-#         img_name = str(sys.argv[3])
-#         if n >= 4:
-#             fps = int(sys.argv[4])
-    
-
-# img = imageio.v2.imread(os.path.join('./', img_name))
-# h, w = img.shape[:2]
-# spacY = h / dimY
-# spacX = w / dimX
-# imagesNames = []
-
-# #forward move
-# for j in range(dimY):
-#     for i in range(dimX):
-#         curImageName = str(j) + 'x' + str(i) + '.png'
-#         imagesNames.append(curImageName)
-#         crop_img = img[int(j*spacY):int((j+1)*(spacY)), int(i*spacX):int((i+1)*(spacX+1))]
-#         imageio.v2.imwrite("cropped/" + str(curImageName), crop_img)
-        
-# #backwards move
-# for j in reversed(range(dimY)):
-#     for i in reversed(range(dimX)):
-#         if ((i == dimY-1 and j == dimX-1) or (i == 0 and j == 0)):
-#             continue
-#         curImageName = str(j) + 'x' + str(i) + '.png'
-#         imagesNames.append(curImageName)
-#         crop_img = img[int(j*spacY):int((j+1)*(spacY)), int(i*spacX):int((i+1)*(spacX+1))]
-#         imageio.v2.imwrite("cropped/" + str(curImageName), crop_img)
-
-# images = list()
-# for filename in imagesNames:
-#     images.append(imageio.v2.imread('./cropped/' + filename))
-# imageio.mimsave('./output.gif', images, format='GIF', fps=fps) 
+# output = mergeImage("1.png", 2)
+# print(f"Merged image saved as: {output}")
